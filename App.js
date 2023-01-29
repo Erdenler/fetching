a8e9dfe432091909c8d7a037ad8f0ae633fa0447
Erdenler <108900630+Erdenler@users.noreply.github.com>
2023-01-29 17:02:46 +0300

    fetching
    person data fetching

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import "./components/Users"
+import Users from './components/Users';
+
+function App() {
+  return (
+    <div className="App">
+      <Users />
+
+    </div>
+  );
+}
+
+export default App;
